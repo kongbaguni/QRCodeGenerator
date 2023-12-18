5a4c7a2eeaca6076fb85f5d4f140f0a310352829
서창열 <kongbaguni@gmail.com>
2023-12-18 17:29:19 +0900

    te

---
+++ QR-code-generator/ThemeManager.swift
@@ -0,0 +1,18 @@
+//
+//  ThemeManager.swift
+//  QR-code-generator
+//
+//  Created by Changyeol Seo on 12/18/23.
+//
+
+import SwiftUI
+
+struct ThemeManager: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ThemeManager()
+}
