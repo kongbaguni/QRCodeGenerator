093474332ed11f8997bc840cef15da9b92f7e297
서창열 <kongbaguni@gmail.com>
2024-04-23 14:11:25 +0900

    Initial Commit

---
+++ QrcodeGenerator2/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  QrcodeGenerator2
+//
+//  Created by Changyeol Seo on 4/23/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ QrcodeGenerator2/QrcodeGenerator2App.swift
@@ -0,0 +1,17 @@
+//
+//  QrcodeGenerator2App.swift
+//  QrcodeGenerator2
+//
+//  Created by Changyeol Seo on 4/23/24.
+//
+
+import SwiftUI
+
+@main
+struct QrcodeGenerator2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
